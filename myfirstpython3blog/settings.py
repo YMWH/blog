4f86b131264ea6289374a5164350783bfe1b4c40
YMWH <yueYing_web@163.com>
2019-03-30 03:52:46 +0800

    设置setting文件

--- myfirstpython3blog/settings.py
+++ myfirstpython3blog/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-n^!voky&ifs^q@f^$px)i3+^z_9v_o!s%0+tyeq88o_xm3p@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-# '127.0.0.1', 'localhost ', '.ymwhweb.com'
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.ymwhweb.com']
 
 # Application definition
 
