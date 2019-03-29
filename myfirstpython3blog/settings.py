9c1294615df88783e4b516205edfb873872d4f42
YMWH <yueYing_web@163.com>
2019-03-30 04:47:29 +0800

    setting第二次修改，位置为STATIC_ROOT

--- myfirstpython3blog/settings.py
+++ myfirstpython3blog/settings.py
@@ -128,13 +128,15 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+        os.path.join(BASE_DIR, 'blogproject/static').replace('\\', '/'),
+        os.path.join(BASE_DIR, 'Course/static').replace('\\', '/'),
+    )
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
-    os.path.join(BASE_DIR, 'blogproject/static').replace('\\', '/'),
-    os.path.join(BASE_DIR, 'Course/static').replace('\\', '/'),
-)
 
 HAYSTACK_CONNECTIONS = {
     'default': {
